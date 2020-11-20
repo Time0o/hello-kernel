d0f9334fa27143412165360e8374e68df552367c
Timo Nicolai <timonicolai@arcor.de>
2020-11-20 19:53:15 +0100

    Add simple GDT

--- boot/asm/boot.asm
+++ boot/asm/boot.asm
@@ -12,6 +12,7 @@ jmp _start
 
 %include "toggle_interrupts.asm"
 %include "a20_enable.asm"
+%include "gdt.asm"
 
 ; TODO relocate bootloader
 ;reloc:
@@ -41,6 +42,8 @@ boot_error:
   jmp boot_error
 
 %include "constants.asm"
+
+GDT
 
 times 510-($-$$) db 0 ; zero out rest of section
 dw BOOT_ID            ; add boot signature (needed by QEMU)
