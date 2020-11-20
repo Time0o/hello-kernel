ba626b5786bac4803dcfb309204c7ab6c0cf7551
Timo Nicolai <timonicolai@arcor.de>
2020-11-20 12:27:18 +0100

    Disable BIOS printing

--- boot/asm/bios_print.asm
+++ boot/asm/bios_print.asm
@@ -1,20 +1,20 @@
-bios_print:
-  lodsb
-  test al, al
-  jz bios_print_done
-  mov ah, 0x0E
-  mov bh, 0
-  int 0x10
-  jmp bios_print
-
-bios_print_done:
-  ret
+;bios_print:
+;  lodsb
+;  test al, al
+;  jz bios_print_done
+;  mov ah, 0x0E
+;  mov bh, 0
+;  int 0x10
+;  jmp bios_print
+;
+;bios_print_done:
+;  ret
 
 %macro BIOS_PRINT 1
-  push si
-
-  mov si, word %1
-  call bios_print
-
-  pop si
+;  push si
+;
+;  mov si, word %1
+;  call bios_print
+;
+;  pop si
 %endmacro
--- boot/asm/constants.asm
+++ boot/asm/constants.asm
@@ -11,10 +11,10 @@ BOOT_SECTORS equ 1
 
 BOOT_STACKSIZE equ KB(32)
 
-%define MSG(NAME, CONTENT) NAME db CONTENT, 0xA, 0xD, 0
-
-MSG(MSG_GREETING, 'Hello from the bootloader')
-MSG(MSG_RELOC_FAILURE, 'Failed to relocate bootloader')
-MSG(MSG_A20_TRY_BIOS, 'Trying to enable A20 line via BIOS interrupt')
-MSG(MSG_A20_SUCCESS, 'Successfully enabled A20 line')
-MSG(MSG_A20_FAILURE, 'Failed to enable A20 line')
+;%define MSG(NAME, CONTENT) NAME db CONTENT, 0xA, 0xD, 0
+;
+;MSG(MSG_GREETING, 'Hello from the bootloader')
+;MSG(MSG_RELOC_FAILURE, 'Failed to relocate bootloader')
+;MSG(MSG_A20_TRY_BIOS, 'Trying to enable A20 line via BIOS interrupt')
+;MSG(MSG_A20_SUCCESS, 'Successfully enabled A20 line')
+;MSG(MSG_A20_FAILURE, 'Failed to enable A20 line')
